340537bd97f6a2ed47830f1f9559ad5a200fc912
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-10-16 22:33:32 +0800

    feat: add request helper

---
+++ request/request_helper.py
@@ -0,0 +1,58 @@
+import urllib.parse
+import requests
+from request_urls import RequestUrls as apiUrls
+from request_signed import RequestSigned
+
+class RequestHelper:
+
+    _HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                              'AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/132.0.0.0 Safari/537.36 '
+                              'MicroMessenger/7.0.20.1781(0x6700143B) '
+                              'NetType/WIFI MiniProgramEnv/Unknown ',
+                'xweb_xhr': '1',
+                'wechat': '1',
+                'Referer': r'https://servicewechat.com/wx9f1c2e0bbc10673c/529/page-frame.html',
+                'Sec-Fetch-Site': 'cross-site',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Dest': 'empty',
+                }
+
+    _REQUEST_SESSION = RequestSigned()
+    _host = ...
+
+
+    def __init__(self, host: str = apiUrls.Host.base):
+        self._REQUEST_SESSION.headers.update(self._HEADERS)
+        self._host = host
+
+
+    def set_config(self, encrypt_value:str|None, jsessionid:str|None):
+        self._REQUEST_SESSION.set_encrypt_value(encrypt_value)
+        self._REQUEST_SESSION.set_jsessionid(jsessionid)
+        return self
+
+
+    def get_config(self):
+        return {'encryptValue': self._REQUEST_SESSION.get_encrypt_value(),
+                'JSESSION': self._REQUEST_SESSION.get_jsessionid(),}
+
+
+    def get(self, url:str, headers: dict[str, str]|None=None) -> requests.Response:
+        headers = self._HEADERS | (headers or {})
+        return self._REQUEST_SESSION.get(self._host + url, headers=headers)
+
+
+    def post(self, url: str,
+             body: dict[str, str]|None=None,
+             as_json=False,
+             headers: dict[str, str]|None=None) -> requests.Response:
+
+        headers = self._HEADERS | (headers or {})
+
+        if as_json or body is None:
+            return self._REQUEST_SESSION.post(self._host + url, json=body or {}, headers=headers)
+
+        else:
+            return self._REQUEST_SESSION.post(self._host + url, data=body, headers=headers)
+
