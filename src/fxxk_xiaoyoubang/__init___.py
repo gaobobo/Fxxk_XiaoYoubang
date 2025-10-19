c819ca6d409539e5405085a56cfb5f6df8395b94
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-10-20 00:48:44 +0800

    feat: add logger

--- src/fxxk_xiaoyoubang/__init___.py
+++ src/fxxk_xiaoyoubang/__init___.py
@@ -1,10 +1,14 @@
 from fxxk_xiaoyoubang.client import Client
 from fxxk_xiaoyoubang.apis import Login, Account, Internship, Clock
+import logging
 
 __all__ = [
     'Client',
     'Login',
     'Account',
     'Internship',
-    'Clock'
+    'Clock',
+    'logger'
 ]
+
+logger = logging.getLogger(__name__)
