60e06ee83bc22a41a9b632493b6f350c2fac6690
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-10-19 23:29:30 +0800

    feat: add top __init__.py

---
+++ src/fxxk_xiaoyoubang/__init___.py
@@ -0,0 +1,10 @@
+from fxxk_xiaoyoubang.apis import XiaoyoubangApis
+from fxxk_xiaoyoubang.calls import Login, Account, Internship, Clock
+
+__all__ = [
+    'XiaoyoubangApis',
+    'Login',
+    'Account',
+    'Internship',
+    'Clock'
+]
