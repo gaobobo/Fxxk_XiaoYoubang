76631cabd11b2c1610d6bddb2ac764feb57ecf25
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-10-20 11:23:17 +0800

    feat: add clock fun

--- src/fxxk_xiaoyoubang/__init__.py
+++ src/fxxk_xiaoyoubang/__init__.py
@@ -1,5 +1,6 @@
 from fxxk_xiaoyoubang.client import Client
 from fxxk_xiaoyoubang.apis import Login, Account, Internship, Clock
+from fxxk_xiaoyoubang.clock import clock
 import logging
 
 __all__ = [
@@ -8,7 +9,8 @@ __all__ = [
     'Account',
     'Internship',
     'Clock',
-    'logger'
+    'logger',
+    'clock'
 ]
 
 logger = logging.getLogger(__name__)
